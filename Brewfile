570f99b1ab35dd29bd1e971320e8104c6ecdad8d
Juan Cruz Martinez <bajcmartinez@gmail.com>
2024-09-06 21:47:37 +0200

    Adding a list of common programs and utils to get installed when running the  setup and also set mac default settings

---
+++ Brewfile
@@ -0,0 +1,54 @@
+# taps
+tap 'homebrew/bundle'
+
+# Packages
+brew 'awscli'
+brew 'coreutils' # Those that come with MacOS are outdated
+brew 'ffmpeg' # For audacity
+brew 'git'
+brew 'jq' # To query files, amazing tool
+brew 'mas' # Install App Store apps through CLI
+brew 'hugo' # Install Hugo
+brew 'stow'
+brew 'fzf'
+brew 'tmux'
+brew 'ripgrep'
+
+# Day to day apps
+cask '1password'
+cask '1password-cli'
+cask 'docker'
+cask 'google-chrome'
+cask 'arc'
+cask 'slack'
+cask 'cleanshot'
+cask 'reader'
+cask 'superhuman'
+cask 'notion'
+cask 'spotify'
+cask 'caffeine'
+
+# Streaming and Recording
+cask 'ecamm-live'
+cask 'audacity'
+cask 'elgato-control-center'
+cask 'elgato-stream-deck'
+cask 'obs'
+
+# Code Editors
+cask 'visual-studio-code'
+cask 'jetbrains-toolbox'
+cask 'webstorm'
+brew 'neovim'
+cask 'pycharm'
+cask 'rustrover'
+
+
+# Fonts
+cask 'font-lato'
+cask 'font-open-sans'
+cask 'font-roboto'
+cask 'font-jetbrains-mono'
+
+# Mac App Store
+
--- nvim/.config/nvim/lua/bajcmartinez/plugins/lsp.lua
+++ nvim/.config/nvim/lua/bajcmartinez/plugins/lsp.lua
@@ -1,61 +1,63 @@
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim', branch = 'v4.x',
-        dependencies = {
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-            {'neovim/nvim-lspconfig'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/nvim-cmp'},
-        },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v4.x",
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/nvim-cmp" },
+		},
 
-        config = function()
-            local lsp_zero = require('lsp-zero')
+		config = function()
+			local lsp_zero = require("lsp-zero")
 
-            local lsp_attach = function(client, bufnr)
-                local opts = {buffer = bufnr}
+			local lsp_attach = function(client, bufnr)
+				local opts = { buffer = bufnr }
 
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-            end
+				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+				vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+				vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+				vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+				vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+				vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+			end
 
-            lsp_zero.extend_lspconfig({
-                sign_text = true,
-                lsp_attach = lsp_attach,
-                capabilities = require('cmp_nvim_lsp').default_capabilities()
-            })
+			lsp_zero.extend_lspconfig({
+				sign_text = true,
+				lsp_attach = lsp_attach,
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			})
 
-            require('mason').setup({})
-            require('mason-lspconfig').setup({
-                ensure_installed = {'lua_ls', 'rust_analyzer', 'tsserver'},
-                handlers = {
-                    function(server_name)
-                        require('lspconfig')[server_name].setup({})
-                    end,
-                }
-            })
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls" },
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
 
-            local cmp = require('cmp')
+			local cmp = require("cmp")
 
-            cmp.setup({
-                sources = {
-                    {name = 'nvim_lsp'},
-                },
-                snippet = {
-                    expand = function(args)
-                    -- You need Neovim v0.10 to use vim.snippet
-                    vim.snippet.expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({}),
-            })
-        end
-    }
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+				snippet = {
+					expand = function(args)
+						-- You need Neovim v0.10 to use vim.snippet
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({}),
+			})
+		end,
+	},
 }
+
