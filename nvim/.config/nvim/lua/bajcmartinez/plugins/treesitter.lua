40c4f55e4d8fb3634652655d81cb72c6d46a4893
Juan Cruz Martinez <bajcmartinez@gmail.com>
2025-05-26 12:07:18 +0200

    feat: more cool stuff

--- nvim/.config/nvim/lua/bajcmartinez/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/bajcmartinez/plugins/treesitter.lua
@@ -1,37 +1,38 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-          ensure_installed = {
-            "help",
-            "bash",
-            "html",
-            "javascript",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "query",
-            "regex",
-            "tsx",
-            "go",
-            "typescript",
-            "vim",
-            "yaml",
-            "rust",
-          },
-          auto_install = true,
-          highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
-        
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-          },
-        },
-      },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"help",
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"go",
+				"typescript",
+				"vim",
+				"yaml",
+				"rust",
+			},
+			auto_install = true,
+			highlight = {
+				-- `false` will disable the whole extension
+				enable = true,
+
+				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				-- Using this option may slow down your editor, and you may see some duplicate highlights.
+				-- Instead of true it can also be a list of languages
+				additional_vim_regex_highlighting = false,
+			},
+		},
+	},
 }
+
